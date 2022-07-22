a336848f8566c4dcedd2bce818cdc89f9e3fd995
codyfenn <fenncody@gmail.com>
2022-07-22 15:08:53 +0000

    Most tests passed

--- ssg.py
+++ ssg.py
@@ -1,9 +1,9 @@
 import typer
 from ssg.site import Site
+import ssg.parsers
 
 def main(source = "content", dest = "dist"):
-	config = {"source":source, "dest":dest}
-	# source,dest = {**config}
+	config = {"source":source, "dest":dest, "parsers":[ssg.parsers.ResourceParser()]}
 	Site(**config).build()
 
 typer.run(main)

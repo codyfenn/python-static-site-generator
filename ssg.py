61988dfe26befa28f4062f86a8dee7d083424103
codyfenn <fenncody@gmail.com>
2022-07-22 13:50:17 +0000

    Passed tests - Site Class

--- ssg.py
+++ ssg.py
@@ -2,9 +2,9 @@ import typer
 from ssg.site import Site
 
 def main(source = "content", dest = "dist"):
-	config = {source:"source", dest:"dest"}
-	source,dest = {**config}
-	Site(source, dest).build()
+	config = {"source":source, "dest":dest}
+	# source,dest = {**config}
+	Site(**config).build()
 
 typer.run(main)
 	
